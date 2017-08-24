53cd4273fc250481096355ed36e23313ffc9db3f
NieYong <aofengblog@163.com>
2017-08-24 17:44:45 +0800

    1、新增方法：复制字符流、复制字符流、将文件内容读取转换成一个字符串。 2、给已有方法增加注释。

--- src/cn/aofeng/common4j/io/IOUtil.java
+++ src/cn/aofeng/common4j/io/IOUtil.java
@@ -1,7 +1,16 @@
 package cn.aofeng.common4j.io;
 
 import java.io.Closeable;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.nio.charset.Charset;
 
 /**
  * I/O实用工具方法集合。
@@ -10,6 +19,12 @@ import java.io.IOException;
  */
 public class IOUtil {
 
+    /** 默认的BUFFER大小：4K  */
+    private final static int BUFFER_SIZE = 4096;
+    
+    /**
+     * 关闭实现了{@link Closeable}接口的对象或资源。
+     */
     public static void closeQuietly(Closeable closeable) {
         if (null != closeable) {
             try {
@@ -18,6 +33,67 @@ public class IOUtil {
                 // nothing
             }
         }
+    }
+
+    /**
+     * 按指定编码读取文件内容并转换成字符串。
+     * 
+     * @param file 待读取的文件对象 
+     * @param readEncoding 读取文件内容的编码格式。如：UTF-8
+     * @return 字符串
+     * @throws IOException 读取文件过程中出错抛出此异常
+     */
+    public static String readAsString(final File file, String readEncoding) throws IOException {
+        StringWriter writer = new StringWriter(BUFFER_SIZE);
+        InputStreamReader reader = null;
+        try {
+            reader = new InputStreamReader(new FileInputStream(file), Charset.forName(readEncoding));
+            copy(reader, writer);
+        } finally {
+            closeQuietly(reader);
+        }
+        
+        return writer.toString();
+    }
+
+    /**
+     * 以字符方式复制数据。
+     * 
+     * @param reader 输入字符流
+     * @param writer 输出字符流
+     * @return 成功复制的字符数
+     * @throws IOException 复制过程中出错抛出此异常
+     */
+    public static long copy(Reader reader, Writer writer) throws IOException {
+        long count = 0;
+        int num = 0;
+        char[] buffer = new char[BUFFER_SIZE];
+        while ( -1 != (num = reader.read(buffer)) ) {
+            writer.write(buffer, 0, num);
+            count += num;
+        }
+        
+        return count;
+    }
+    
+    /**
+     * 以字节方式复制数据。
+     * 
+     * @param ins 输入流
+     * @param outs 输出流
+     * @return 成功复制的字节数
+     * @throws IOException 复制过程中出错抛出此异常
+     */
+    public static long copy(InputStream ins, OutputStream outs) throws IOException {
+        long count = 0;
+        int num = 0;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        while ( -1 != (num = ins.read(buffer)) ) {
+            outs.write(buffer, 0, num);
+            count += num;
+        }
+        
+        return count;
     }
 
 }
