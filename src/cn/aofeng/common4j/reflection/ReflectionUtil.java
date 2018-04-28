4f0cc139d6cef5031da66ecf93addebb7cd42864
NieYong <aofengblog@163.com>
2018-04-28 15:56:50 +0800

    新增：加载Class可指定ClassLoader

--- src/cn/aofeng/common4j/reflection/ReflectionUtil.java
+++ src/cn/aofeng/common4j/reflection/ReflectionUtil.java
@@ -26,25 +26,41 @@ public class ReflectionUtil {
      * @param className 完整类名
      * @return 类实例。如果找不到指定的类名，将返回false。
      */
-    @SuppressWarnings("unchecked")
     public static <T> T createInstance(String className) {
+        return createInstance(className, null);
+    }
+    
+    /**
+     * 生成指定完整类名的实例。
+     * 
+     * @param className 完整类名
+     * @param loader 加载类的{@link ClassLoader}
+     * @return 类实例。如果找不到指定的类名，将返回false。
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T createInstance(String className, ClassLoader loader) {
         if (null == className ) {
             return null;
         }
         
-        T listener = null;
+        T obj = null;
         try {
-            Class<T> listenerClass = (Class<T>) Class.forName(className);
-            Constructor<T> constructor = listenerClass.getDeclaredConstructor();
+            Class<T> claz = null;
+            if (null == loader) {
+                claz = (Class<T>) Class.forName(className);
+            } else {
+                claz = (Class<T>) Class.forName(className, true, loader);
+            }
+            Constructor<T> constructor = claz.getDeclaredConstructor();
             constructor.setAccessible(true);
-            listener = constructor.newInstance();
+            obj = constructor.newInstance();
         } catch (ClassNotFoundException e) {
             _logger.error( String.format("could not found class:%s", className), e);
         } catch (Exception e) {
             _logger.error( String.format("create instance for class:%s fail", className), e);
         }
         
-        return listener;
+        return obj;
     }
     
     /**
@@ -55,26 +71,45 @@ public class ReflectionUtil {
      * @param initargs 调用构造方法生成实例时作为参数传给构造方法的对象数组
      * @return 类实例。如果找不到指定的类名，将返回false。
      */
-    @SuppressWarnings("unchecked")
     public static <T> T createInstance(String className, 
             Class<?>[] parameterTypes, Object[] initargs) {
+        return createInstance(className, parameterTypes, initargs, null);
+    }
+    
+    /**
+     * 生成指定完整类名的实例。
+     * 
+     * @param className 完整类名
+     * @param parameterTypes 构造参数类型数组
+     * @param initargs 调用构造方法生成实例时作为参数传给构造方法的对象数组
+     * @param loader 加载类的{@link ClassLoader}
+     * @return 类实例。如果找不到指定的类名，将返回false。
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T createInstance(String className, 
+            Class<?>[] parameterTypes, Object[] initargs, ClassLoader loader) {
         if (null == className ) {
             return null;
         }
         
-        T listener = null;
+        T obj = null;
         try {
-            Class<T> listenerClass = (Class<T>) Class.forName(className);
-            Constructor<T> constructor = listenerClass.getDeclaredConstructor(parameterTypes);
+            Class<T> claz = null;
+            if (null == loader) {
+                claz = (Class<T>) Class.forName(className);
+            } else {
+                claz = (Class<T>) Class.forName(className, true, loader);
+            }
+            Constructor<T> constructor = claz.getDeclaredConstructor(parameterTypes);
             constructor.setAccessible(true);
-            listener = constructor.newInstance(initargs);
+            obj = constructor.newInstance(initargs);
         } catch (ClassNotFoundException e) {
             _logger.error( String.format("could not found class:%s", className), e);
         } catch (Exception e) {
             _logger.error( String.format("create instance for class:%s fail", className), e);
         }
         
-        return listener;
+        return obj;
     }
 
     /**
